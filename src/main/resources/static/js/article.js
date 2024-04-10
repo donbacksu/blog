603d144657aa21c986e1a2ac6721e4658663c359
donbacksu <156149926+donbacksu@users.noreply.github.com>
2024-04-10 19:09:49 +0900

    두영
    깃헙 데스크톱 처음

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -39,3 +39,27 @@ if (modifyButton) {
             });
     });
 }
+
+// id 가 create-btn 인 엘리먼트
+const createButton = document.getElementById("create-btn");
+
+if (createButton){
+//     클릭 이벤트가 감지되면 생성 API 요청
+    createButton.addEventListener("click", (event) => {
+        fetch("/api/articles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value,
+            }),
+        }).then(() => {
+            alert("등록이 안료되었습니다.");
+            location.replace("/articles");
+        });
+    });
+}
+
+
