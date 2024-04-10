b42f53625462446eca707b9d8cb1974526407a32
donbacksu <156149926+donbacksu@users.noreply.github.com>
2024-04-10 17:12:18 +0900

    Initial commit

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,41 @@
+// 삭제기능
+const deleteButton = document.getElementById('delete-btn');
+
+if (deleteButton) {
+    deleteButton.addEventListener('click', event => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/article/${id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                alert('삭제가 완료되었습니다.');
+                location.replace('/articles');
+            });
+    });
+}
+
+// 수정기능
+// id가 modify-btn인 앨리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+            .then(() => {
+                alert('수정이 완료되었습니다.');
+                location.replace(`/articles/${id}`);
+            });
+    });
+}
